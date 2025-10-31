d725db8200ccff5a0a10eb33b597f9fdcde89168
qorcodusl <qorcodus03@naver.com>
2025-10-31 22:46:42 +0900

    fix(view) : InputView → InputParser 호출 구조로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,41 +1,26 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 import java.util.List;
+import lotto.util.InputParser;
 
 public class InputView {
 
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        try {
-            return Integer.parseInt(input.trim());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 숫자를 입력해 주세요.");
-        }
+        return InputParser.parseSingleNumber(input);
     }
 
     public static List<Integer> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해주세요");
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
-        try{
-            return Arrays.stream(input.split(","))
-                    .map(String::trim)
-                    .map(Integer::parseInt)
-                    .toList();
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
-        }
+        return InputParser.parseNumbers(input);
     }
 
     public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
-        try {
-            return Integer.parseInt(input.trim());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
-        }
+        return InputParser.parseSingleNumber(input);
     }
 }
